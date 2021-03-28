3ac7c4e70844df5bf6bbda4d764eaa1e3ada3725
Melissa Blades <melissa@saltymermaid.dev>
2021-03-28 10:00:33 -0400

    Add all modal components

--- js/app.js
+++ js/app.js
@@ -13,71 +13,89 @@ function createWithTextAndAddToElement(elem, content, classy, adult) {
   return element;
 }
 
+function createLinkAndAddToElement(link, classy, adult) {
+  let element = document.createElement('a');
+  element.setAttribute('href', link);
+  element.setAttribute('class', classy)
+  element.setAttribute('target', '_blank')
+  adult.appendChild(element);
+  return element;
+}
+
 const bubbleContainer = document.querySelector(".project-list");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal-close");
+const modalNav = document.querySelector(".modal-nav");
+const modalNext = document.querySelector(".modal-next");
+const modalPrev = document.querySelector(".modal-prev");
 
 function createBubbles() {
-  for (let i = 1; i < 8; i ++) {
+  for (let i = 1; i < 9; i ++) {
     const bubble = createAndAddToElement('div', 'class', `project-bubble proj-${i}`, bubbleContainer)
     bubble.setAttribute('data-index', i)
-    createWithTextAndAddToElement('p', projectList[i].name, 'bubble-text', bubble)
+    createWithTextAndAddToElement('p', projectList[i-1].name, 'bubble-text', bubble)
   }
 }
 
 createBubbles()
 
-// function createBubble(index, grandparent, parentClass, modal=false) {
-//   const project = projectList[index]
-//   const parent = createAndAddToElement('div', 'class', parentClass, grandparent)
-//   parent.setAttribute('data-index', index)
-//   const photo = createAndAddToElement('img', 'src', project.image, parent)
-//   photo.setAttribute('class', 'projectImage')
-//   const textContainer = createAndAddToElement('div', 'class', 'text-container', parent)
-//   if (!modal) {
-//     createWithTextAndAddToElement('h2', `${name.first} ${name.last}`, 'name', textContainer)
-//   }
-//   createWithTextAndAddToElement('p', email, 'email', textContainer)
-//   createWithTextAndAddToElement('p', city, 'location', textContainer)
-//   if (modal) {
-//     let hr = document.createElement('hr');
-//     textContainer.appendChild(hr);
-//     createWithTextAndAddToElement('p', cell, 'additional', textContainer )
-//     createWithTextAndAddToElement(
-//       'p',
-//       `${street.number} ${street.name}, ${city}, ${state} ${postcode}`,
-//       'additional',
-//       textContainer
-//     )
-//     createWithTextAndAddToElement('p', `Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`, 'additional', textContainer )
-//   }
-// }
+function createModal(index, grandparent, parentClass, modal=false) {
+  const project = projectList[index - 1]
+  const parent = createAndAddToElement('div', 'class', parentClass, grandparent)
+  parent.setAttribute('data-index', index)
+  const photo = createLinkAndAddToElement(project.url, 'proj-link', parent)
+  createAndAddToElement('img', 'src', project.image, photo)
+  photo.setAttribute('class', 'projectImage')
+  const textContainer = createAndAddToElement('div', 'class', 'text-container', parent)
+  createWithTextAndAddToElement('p', project.description, 'description', textContainer)
+  const list = createAndAddToElement('ul', 'class', 'topics', textContainer)
+  project.topics.forEach(topic => {
+    createWithTextAndAddToElement('li', topic, 'topic', list)
+  })
+  const bottomRow = createAndAddToElement('div', 'class', 'bottom-row', parent)
+  const skills = createAndAddToElement('ul', 'class', `skills skill-proj-${index}`, bottomRow)
+  project.skills.forEach(skill => {
+    const skillClass = skill.toLowerCase()
+    createWithTextAndAddToElement('li', skill, `badge rounded-pill ${skillClass}`, skills)
+  })
+  const ghLink = createLinkAndAddToElement(project.github, `logo logo-proj-${index}`, bottomRow)
+  ghLink.innerHTML = githubLogo
+}
 
-// function displayModal(index) {
-//   const navButtons = Array.from(modalNav.children);
-//   navButtons.forEach(button => button.classList.remove("hidden"));
-//   const deleteModal = modal.querySelector('.modal-content');
-//   if (deleteModal) modal.removeChild(deleteModal);
-//   createEmployee(index, modal, 'modal-content', true)
-//   displayModalNav(index);
-//   overlay.classList.remove("hidden");
-// }
+function displayModal(index) {
+  const navButtons = Array.from(modalNav.children);
+  navButtons.forEach(button => button.classList.remove("hidden"));
+  const deleteModal = modal.querySelector('.modal-content');
+  if (deleteModal) modal.removeChild(deleteModal);
+  createModal(index, modal, 'modal-content', true)
+  displayModalNav(index);
+  overlay.classList.remove("hidden");
+}
 
-// gridContainer.addEventListener('click', e => {
-//   if (e.target !== gridContainer) {
-//     const card = e.target.closest(".card");
-//     const index = parseInt(card.getAttribute('data-index'));
-//     displayModal(index);
-//     }
-//   });
+function displayModalNav(index) {
+  const size = projectList.length;
+  if (index === size - 1) modalNext.classList.add("hidden");
+  if (index === 0) modalPrev.classList.add("hidden");
+}
 
-// modalClose.addEventListener('click', () => {
-//   overlay.classList.add("hidden");
-// });
+bubbleContainer.addEventListener('click', e => {
+  if (e.target !== bubbleContainer) {
+    const bubble = e.target.closest(".project-bubble");
+    const index = parseInt(bubble.getAttribute('data-index'));
+    displayModal(index);
+    }
+  });
 
-// overlay.addEventListener('click', (e) => {
-//   if (e.target === overlay) {
-//     overlay.classList.add("hidden");
-//   }
-// });
+modalClose.addEventListener('click', () => {
+  overlay.classList.add("hidden");
+});
+
+overlay.addEventListener('click', (e) => {
+  if (e.target === overlay) {
+    overlay.classList.add("hidden");
+  }
+});
 
 // modalNav.addEventListener('click', (e) => {
 //   const employeeIndex = document.querySelector('.modal-content');
--- js/projects.js
+++ js/projects.js
@@ -5,7 +5,7 @@ const projectList = [
     github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
     url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
     image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I added things like additional pages and a CSS transition.',
     topics: [
       'Semantic HTML',
       'CSS transitions'
@@ -16,15 +16,17 @@ const projectList = [
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    name: 'Responsive Portfolio',
+    title: 'Project 2 | Responsive Portfolio',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-2',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-2/',
+    image: 'images/proj-2-responsive-portfolio.png',
+    description: 'This was my first introduction to responsive design and a mobile-first approach. This project was completed with only basic CSS - we were not meant to use flex-box or CSS grid. For the project, I was given three mockups of the design for mobile, tablet, and desktop screens as well as the images and text. Things I learned:',
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Designing with a mobile-first approach',
+      'Creating a responsive layout with media queries',
+      'Using the CSS box model',
+      'Adding fonts (from Google fonts)'
     ],
     skills: [
       'HTML',
@@ -32,15 +34,16 @@ const projectList = [
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    name: 'Registration Form',
+    title: 'Project 3 | An Online Registration Form',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-3',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-3/',
+    image: 'images/proj-3-online-registration-form.png',
+    description: "This was one of the hardest projects for me. This was my first introduction to CSS Flex Box and I spent a lot of time shaking my fist in the air and hurtling obscenities at my computer. I finally made peace with CSS when I accepted that if my button was in the wrong place it was because I did something to cause it to be in the wrong place. I really honed my Chrome dev tools skills working on this project. Although I've worked with forms before, using the pure HTML form fields allowed me to really understand how they work.",
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Using Chrome dev tools to decipher CSS',
+      'Using CSS Flex Box',
+      'Using HTML forms'
     ],
     skills: [
       'HTML',
@@ -48,83 +51,102 @@ const projectList = [
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
+    name: 'Style Guide',
+    title: 'Project 4 | Web Style Guide',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-4',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-4/',
+    image: 'images/proj-4-web-style-guide.png',
     description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Implementing Sass variables',
+      'Nesting CSS Selectors',
+      'Creating partials',
+      'Using mixins',
+      'Using operators'
     ],
     skills: [
       'HTML',
-      'CSS'
+      'CSS',
+      'Sass'
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    name: 'Photo Gallery',
+    title: 'Project 5 | An Interactive Photo Gallery',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-5',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-5/',
+    image: 'images/proj-5-interactive-photo-gallery.png',
+    description: 'For the Photo Gallery project, I used a lightbox plug-in, baguetteBox. A search box was required - for extra credit I made it from scratch.',
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Creating a search to filter images based on captions',
+      'Introduction to CSS Grid',
+      'Implementing a Javascript plugin',
+      'Updating HTML with Javascript (to hide filtered images)'
     ],
     skills: [
       'HTML',
-      'CSS'
+      'CSS',
+      'Javascript'
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    name: 'Wheel of Success Game',
+    title: 'Project 6 | Wheel of Success Game',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-6',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-6/',
+    image: 'images/proj-6-wheel-of-success.png',
+    description: 'For this project, I was given the HTML and CSS and needed to add the Javascript to make the game work. This was my first experience with adding, removing, and changing elements via Javascript. Things I learned:',
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Finding elements with CSS selectors',
+      'Modifying HTML element attributes via Javascript',
+      'More experience working with event listeners',
+      'Using Chrome dev tools to debug javascript'
     ],
     skills: [
       'HTML',
-      'CSS'
+      'CSS',
+      'Javascript'
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    name: 'WebApp Dashboard',
+    title: 'Project 7 | WebApp Dashboard',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-7',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-7/',
+    image: 'images/proj-7-webapp-dashboard.png',
+    description: 'This was the hardest and most time-consuming project for me. The page design required creating many different elements - from charts to social media widgets to messages and toggles - each one with its own complexities. The actual charts use chart.js, but everything else is homemade. I learned so much working on this page. I will probably always use a library for many of these elements in the future, but it was a great experience to code them from scratch one time:',
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Creating an auto-complete search box',
+      'Creating toggle switches',
+      'Saving to local storage (Settings section) ',
+      'Changing the color of SVG files',
+      'Creating an alert on the page',
+      'Using keyframes to animate an element (see the bell) ',
+      'Using CSS grid for responsive design'
     ],
     skills: [
       'HTML',
-      'CSS'
+      'CSS',
+      'Sass',
+      'Javascript'
     ]
   },
   {
-    name: 'Profile Page',
-    title: 'Project 1 | My Personal Profile Page',
-    github: 'https://github.com/saltymermaid/fewd-tech-degree-1',
-    url: 'https://saltymermaid.github.io/fewd-tech-degree-1/',
-    image: 'images/proj-1-personal-profile.png',
-    description: 'This was the Hello, World project for the program. I was given a complete profile page and the assignment was to update it with my own text and images. For extra credit, I add things like additional pages and a CSS transition.',
+    name: 'Employee Directory',
+    title: 'Project 8 | Use an API to Create an Employee Directory',
+    github: 'https://github.com/saltymermaid/fewd-techdegree-8',
+    url: 'https://saltymermaid.github.io/fewd-techdegree-8/',
+    image: 'images/proj-8-api-employee-directory.png',
+    description: "Following the intense dashboard project, this project was a fun reprieve. I used a random user generator at https://randomuser.me/. While I didn't learn many new concepts in this project, I gained more experience with CSS and Javascript. Things I learned:",
     topics: [
-      'Semantic HTML',
-      'CSS transitions'
+      'Creating a modal from scratch',
+      'Filtering transient data and then using that data to navigate through the filtered users in the modal'
     ],
     skills: [
       'HTML',
-      'CSS'
+      'CSS',
+      'Sass',
+      'Javascript'
     ]
   }
 ]
